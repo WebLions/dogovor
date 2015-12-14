6c1d27c67f21121abf8ccff4d4ce6f8173277915
ArgeSwarge <argecata@mail.ru>
2015-12-14 22:38:49 +0200

    sdfsdf

--- application/views/user/footer.php
+++ application/views/user/footer.php
@@ -2,29 +2,14 @@
     <?php echo link_tag('bootstrap/js/jquery-ui-1.10.4.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/jquery-1.8.3.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/jquery-ui-1.9.2.custom.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/bootstrap.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery.scrollTo.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery.nicescroll.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery.sparkline.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/owl.carousel.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/fullcalendar.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/calendar-custom.js').'<br>';?>
+    <?php echo link_tag('bootstrap/js/bootstrap.min.js').'<br>';?>   
+    <?php echo link_tag('bootstrap/js/jquery.sparkline.js').'<br>';?>   
     <?php echo link_tag('bootstrap/js/jquery.rateit.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/jquery.customSelect.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/scripts.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/sparkline-chart.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/easy-pie-chart.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery-jvectormap-1.2.2.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery-jvectormap-world-mill-en.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/xcharts.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/jquery.autosize.min.js').'<br>';?>
     <?php echo link_tag('bootstrap/js/jquery.placeholder.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/gdp-data.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/morris.min.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/sparklines.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/charts.js').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery.knob').'<br>';?>
-    <?php echo link_tag('bootstrap/js/jquery.slimscroll.min.js').'<br>';?>
+   
 
 
   <script>
