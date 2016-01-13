65a1a64ce0f7113800f2812f1d415b6050d09c86
ArgeSwarge <argecata@mail.ru>
2016-01-13 21:06:52 +0200

    Сгрупировал блоки/поменял имена согласно списка*все/цвет placeholdera поменял,его теперь виднее/выровнял по левому краю/обернул документ в форму/ группы radio поменял

--- application/views/blocks/main_block_bs.php
+++ application/views/blocks/main_block_bs.php
@@ -95,7 +95,7 @@
                     <input class = "form-control" type="text" name="vendor_flat"  placeholder="Квартира:">
                 </div>
                 <div class = "content-input-group">
-                    <input class="form-control" type="text" name="qwe"  placeholder="vendor_phone:">
+                    <input class="form-control" type="text" name="vendor_phone"  placeholder="Телефон:">
                 </div>
             </div>
         </div>
